bc1cd6b12d6252776a77f1fe3a37a6cd41f52a19
joshclark756 <joshclark756@gmail.com>
2023-02-05 18:21:13 -0600

    Update and rename ! backpack play.lua to forever play.lua

--- forever play.lua
+++ forever play.lua
@@ -1,3 +1,4 @@
+---play and sound then execute sound will play forever afterwards 
 for i,v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do 
     if v:isA("Tool") then v.Server:Destroy()
     end
