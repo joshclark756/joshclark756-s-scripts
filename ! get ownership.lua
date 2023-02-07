bc975a791958f6220c917dd26a06186de3f0d9fc
joshclark756 <joshclark756@gmail.com>
2023-02-06 18:57:20 -0600

    Update ! get ownership.lua

--- ! get ownership.lua
+++ ! get ownership.lua
@@ -1,3 +1,4 @@
+--- tps primarypart to parts
 for i,v in next, game:GetDescendants() do
     if v:isA("Part") and v.Anchored == false then do 
 game.Players.LocalPlayer.Character.PrimaryPart.CFrame = v.CFrame
