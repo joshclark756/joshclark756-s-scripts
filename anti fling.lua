f64f8fac88d9a1a0b598c3ff093ffaf06cff8f24
joshclark756 <joshclark756@gmail.com>
2023-02-06 18:43:49 -0600

    Create anti fling.lua

---
+++ anti fling.lua
@@ -0,0 +1,41 @@
+-- init
+if not game.IsLoaded(game) then
+   repeat task.wait() until game.IsLoaded(game)
+end
+
+-- variables
+local RunService, FindFirstChildOfClass = game.GetService(game, "RunService"), game.FindFirstChildOfClass;
+local Client = game.GetService(game, "Players").LocalPlayer
+
+-- functions
+function ModifyVelocity()
+   local HeartbeatLoop = nil
+   
+   for _, v in next, game:GetDescendants() do
+       if  v and v:IsA"Part"  and v.Parent ~= Client.Character and v.Anchored == false and v.Name == "HumanoidRootPart" then 
+           HeartbeatLoop = RunService.Heartbeat:Connect(function()
+               v.CustomPhysicalProperties = PhysicalProperties.new(0,0,0,0,0)
+               v.Velocity = Vector3.new(0,0,0)
+               v.RotVelocity = Vector3.new(0,0,0)
+               v.CanCollide = false
+           end)
+       end
+   end
+   end
+   
+   Client.Character.Humanoid.Died:Connect(function()
+       HeartbeatLoop:Disconnect()
+       HeartbeatLoop = nil
+   end)
+ModifyVelocity()
+workspace.DescendantAdded:Connect(function(part) if part:isA("Part") and part.Name == "HumanoidRootPart" then do  ModifyVelocity()
+function CharacterAddedEvent(Character)
+   repeat task.wait() until FindFirstChildOfClass(Character, "Humanoid")
+   
+  ModifyVelocity()
+  
+end
+Client.CharacterAdded:Connect(CharacterAddedEvent)
+end
+end
+end)
