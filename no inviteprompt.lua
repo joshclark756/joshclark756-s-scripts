4d7080a3be0c310120ee219ac18d0ff1ae03bb3b
joshclark756 <joshclark756@gmail.com>
2023-02-11 15:54:05 -0600

    Update no inviteprompt.lua

--- no inviteprompt.lua
+++ no inviteprompt.lua
@@ -1,3 +1,4 @@
+-- made for those games that annoy you with inivte prompt autoexec friendly
 while wait() do
 for i,v in pairs(game.CoreGui:GetDescendants()) do
     if v:IsA("ScreenGui") then if  v.Name == "invitePrompt" then do
