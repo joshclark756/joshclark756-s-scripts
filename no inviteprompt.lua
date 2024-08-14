7aaa2f3d10cea409f0787fb20211686122fa45cc
joshclark756 <joshclark756@gmail.com>
2024-08-13 23:18:49 -0500

    Update no inviteprompt.lua

--- no inviteprompt.lua
+++ no inviteprompt.lua
@@ -2,7 +2,7 @@
 while wait() do
 for i,v in pairs(game.CoreGui:GetDescendants()) do
     if v:IsA("ScreenGui") then if  v.Name == "invitePrompt" then do
-        v:destroy() end else end
+        v:destroy() end else game:GetService("CoreGui").GameInvite:Destroy() end
     end
     end
     end
