09497516a410100d72c6302f375e0fd50872215e
joshclark756 <joshclark756@gmail.com>
2025-01-02 10:49:54 -0600

    Create reset filter.lua

---
+++ reset filter.lua
@@ -0,0 +1,34 @@
+-- made by joshclark756
+-- resets the filter
+local Players = game:GetService('Players')
+local UserInputService = game:GetService('UserInputService')
+local LocalPlayer = Players.LocalPlayer
+
+local function generateRandomString()
+    local chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    local length = math.random(80, 90)
+    local randomString = ""
+    for i = 1, length do
+        local randomIndex = math.random(1, #chars)
+        randomString = randomString .. string.sub(chars, randomIndex, randomIndex)
+    end
+    return randomString
+end
+
+local function modifyString(randomText)
+    local modified = ""
+    for char in randomText:gmatch(".") do
+        if char ~= " " then
+            modified = modified .. char .. ""
+        end
+    end
+    return modified
+end
+
+UserInputService.InputBegan:Connect(function(input, gameProcessedEvent)
+    if not gameProcessedEvent and input.KeyCode == Enum.KeyCode.R then
+        local randomText = generateRandomString()
+        local modified = modifyString(randomText)
+        game.Players:Chat("/e " .. modified)
+    end
+end)
